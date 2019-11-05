0be83b09144ec4423fd6d90bd6d1f6d7e5c9f413
Pete Hanner <petehanner@gmail.com>
2019-11-05 17:02:11 -0500

    sidebar

--- src/components/CourseSidebar.js
+++ src/components/CourseSidebar.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import { withRouter } from "react-router-dom";
 
 const CourseSidebar = (props) => {
+
+  const goHome = (e) => {
+    e.preventDefault()
+    props.history.push(`/`)
+  }
+
   return(
     <div className='sidenav'>
-      Course Sidebar
+      <h4>Add An Assignment</h4>
+      <Button
+        variant='outline-primary'
+        size='sm'
+        onClick={goHome}
+      >
+        {`<<  Home`}
+      </Button>
     </div>
   )
 }
 
-export default CourseSidebar
+export default withRouter(CourseSidebar)
