0040f649b29e92cc141570d2796b1b88172c0f46
Pete Hanner <petehanner@gmail.com>
2019-11-13 09:58:53 -0500

    data formatted (i believe) correctly

--- src/components/ScheduleSidebar.js
+++ src/components/ScheduleSidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import { withRouter } from "react-router-dom";
+import { ExportToCsv } from 'export-to-csv';
+
 
 const ScheduleSideBar = (props) => {
 
@@ -9,9 +11,42 @@ const ScheduleSideBar = (props) => {
     props.history.push(`/`)
   }
 
-  return(
+  const csvExport = (e) => {
+    const data = []
+    props.assignments.forEach(date => {
+      const asgmts = date[1]
+      asgmts.forEach(a => {
+        const fields = {
+          task: a.description,
+          list: a.course.name,
+          duedate: a.adj_date
+        }
+        data.push(fields)
+      })
+    })
+    console.log(data)
+
+    const options = {
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalSeparator: '.',
+      showLabels: true,
+      showTitle: true,
+      title: 'My Awesome CSV',
+      useTextFile: false,
+      useBom: true,
+      headers: ['TASK', 'LIST', 'DUEDATE']
+    };
+  }
+
+  return (
     <div className='sidenav'>
-      <h4>Export To App</h4>
+      <h4
+        id='export-button'
+        onClick={csvExport}
+      >
+        Export To App
+      </h4>
       <Button
         variant='outline-primary'
         size='sm'
