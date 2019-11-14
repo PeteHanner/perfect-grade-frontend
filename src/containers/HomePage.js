b87f4c01d939d1affdebde3c5c27076dafc76d70
Pete Hanner <petehanner@gmail.com>
2019-11-14 11:20:54 -0500

    new new logo

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -9,8 +9,7 @@ class HomePage extends Component {
   render() {
     return (
       <div className='offside' id='home-page'>
-        <img id='main-logo' src='https://i.imgur.com/yh4Je8f.png' />
-        {/* <h1 id='main-header'>Perfect Grade</h1> */}
+        <img id='main-logo' src='https://i.imgur.com/yAkZCHP.png' />
         {
           this.props.loading ?
             <h5>Loading...</h5>
