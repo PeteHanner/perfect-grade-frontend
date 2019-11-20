bdcf6561c286ce50c704fe3ee21a2631d0231df1
Pete Hanner <petehanner@gmail.com>
2019-11-20 10:32:28 -0500

    ESLint

--- src/components/CourseCards.js
+++ src/components/CourseCards.js
@@ -1,41 +1,43 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import NewCourseForm from "./NewCourseForm";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import NewCourseForm from './NewCourseForm';
 
-const CourseCards = props => {
+const CourseCards = (props) => {
   const colorArray = [
-    "a20705",
-    "610A95",
-    "B69500",
-    "156D20",
-    "005C76",
-    "c26702"
+    'a20705',
+    '610A95',
+    'B69500',
+    '156D20',
+    '005C76',
+    'c26702',
   ];
 
-  const goToCoursePage = e => {
+  const goToCoursePage = (e) => {
     e.preventDefault();
     props.history.push(`/courses/${e.target.dataset.id}`);
   };
 
-  const renderCourseCards = () => {
-    return props.courses.map((course, index) => {
-      const bgColor = "#" + colorArray[index % 6];
-      // debugger
-      return (
-        <div
-          className="course-card"
-          key={course.id}
-          data-id={course.id}
-          style={{ backgroundColor: bgColor }}
-          onClick={goToCoursePage}
-        >
-          <h2 className="card-title" data-id={course.id}>
-            {course.name}
-          </h2>
-        </div>
-      );
-    });
-  };
+  const renderCourseCards = () => props.courses.map((course, index) => {
+    const bgColor = `#${colorArray[index % 6]}`;
+    // debugger
+    return (
+      <div
+        className="course-card"
+        key={course.id}
+        data-id={course.id}
+        style={{ backgroundColor: bgColor }}
+        onClick={goToCoursePage}
+      >
+        <h2 className="card-title" data-id={course.id}>
+          {course.name}
+        </h2>
+      </div>
+    );
+  });
 
   const [newCourseShow, setNewCourseShow] = React.useState(false);
 
@@ -44,8 +46,8 @@ const CourseCards = props => {
       {renderCourseCards()}
       <div
         className="course-card"
-        style={{ backgroundColor: "lightgray" }}
-        onClick={e => setNewCourseShow(true)}
+        style={{ backgroundColor: 'lightgray' }}
+        onClick={() => setNewCourseShow(true)}
       >
         <h2 className="card-title new-course">Add a New Course</h2>
       </div>
