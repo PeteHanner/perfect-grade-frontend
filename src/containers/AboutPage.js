2f808366cdf576da26f9e7b11c6f5a68a423df62
Pete Hanner <petehanner@gmail.com>
2019-12-15 13:23:51 -0500

    intro copy

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-filename-extension */
@@ -17,7 +18,14 @@ class AboutPage extends Component {
       <div className="offside about-page">
         <h1>Perfect Grade</h1>
         <p className="subtitle">So the semester never gets too steep.</p>
+        <hr />
 
+        <p>
+          We all know the feeling. You <em>finally</em> get through that huge,
+          stressful chunk of coursework and get a chance to relax. You only have
+          a couple easy assignments due over the next couple days. You&apos;re
+          living easy!
+        </p>
 
         <Button
           className="home-btn"
