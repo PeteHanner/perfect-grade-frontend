07c085b0016bcd73e0bcb1b083499910a5552ddc
Pete Hanner <petehanner@gmail.com>
2019-12-21 10:34:59 -0500

    staging assignment reducer

--- src/reducers/assignmentsReducer.js
+++ src/reducers/assignmentsReducer.js
@@ -1,60 +1,70 @@
 export default function assignmentsReducer(
   state = {
     assignments: [],
-    requesting: false
+    requesting: false,
+    error: null,
   },
-  action
+  action,
 ) {
   switch (action.type) {
     case 'REQUESTING_ASSIGNMENTS':
       return {
         ...state,
         assignments: [...state.assignments],
-          requesting: true
+        requesting: true,
       };
     case 'ASSIGNMENTS_LOADED':
       return {
         ...state,
         assignments: action.payload,
-          requesting: false
+        requesting: false,
+        error: null,
       };
     case 'CREATING_ASSIGNMENT':
       return {
         ...state,
         assignments: [...state.assignments],
-          requesting: true
+        requesting: true,
       };
     case 'ASSIGNMENT_CREATED':
       return {
         ...state,
         assignments: [...state.assignments, action.payload],
-          requesting: false
+        requesting: false,
+        error: null,
       };
     case 'EDITING_ASSIGNMENT':
       return {
         ...state,
         assignments: [...state.assignments],
-          requesting: true,
+        requesting: true,
       };
     case 'ASSIGNMENT_EDITED':
       return {
         ...state,
         assignments: [...state.assignments, action.payload],
-          requesting: false
+        requesting: false,
+        error: null,
       };
     case 'DELETING_ASSIGNMENT':
       return {
         ...state,
         assignments: [...state.assignments],
-          requesting: true
+        requesting: true,
       };
     case 'ASSIGNMENT_DELETED':
-    return {
-      ...state,
-      assignments: [...state.assignments.filter(a => a.id !== action.payload.id)],
-      requesting: false
-    };
+      return {
+        ...state,
+        assignments: [...state.assignments.filter((a) => a.id !== action.payload.id)],
+        requesting: false,
+        error: null,
+      };
+    case 'BAD_REQUEST':
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
-};
+}
--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -71,7 +71,7 @@ export default function coursesReducer(
       return {
         ...state,
         error: action.payload,
-      }
+      };
     default:
       return state;
   }
