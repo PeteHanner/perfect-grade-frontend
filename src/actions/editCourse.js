833239e1fc467a10bab554a58345ee0118102b7c
Pete Hanner <petehanner@gmail.com>
2020-01-03 14:40:22 -0500

    async conversion

--- src/actions/editCourse.js
+++ src/actions/editCourse.js
@@ -14,17 +14,20 @@ export function editCourse(formData) {
     body: JSON.stringify(formData),
   };
 
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch({ type: 'EDITING_COURSE' });
-    fetch(`http://localhost:3001/courses/${formData.courseId}`, configObj)
-      .then((r) => r.json())
-      .then((newCourse) => {
-        dispatch({ type: 'COURSE_EDITED', payload: newCourse });
-        dispatch({ type: 'CHANGES_MADE' });
-        dispatch(fetchCourses());
-      })
-      .catch((error) => {
-        dispatch({ type: 'BAD_REQUEST', payload: error.message });
-      });
+
+    try {
+      const r = await fetch(`http://localhost:3001/courses/${formData.courseId}`, configObj);
+      const newCourse = await r.json();
+      dispatch({ type: 'COURSE_EDITED', payload: newCourse });
+      dispatch({ type: 'CHANGES_MADE' });
+      dispatch(fetchCourses());
+      return true;
+    }
+    catch (error) {
+      dispatch({ type: 'BAD_REQUEST', payload: error.message });
+      return false;
+    }
   };
 }
